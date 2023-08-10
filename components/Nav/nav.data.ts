f8b65547f4d852b81163ac86a880e540e0c70e0d
TheIsrael1 <ehindero2022@gmail.com>
2023-08-10 02:23:14 +0100

    chore - added reveiws link

--- components/Nav/nav.data.ts
+++ components/Nav/nav.data.ts
@@ -1,5 +1,6 @@
 export const navData: {title: string, link: string, section: boolean}[] = [
     {link: 'https://docs.google.com/document/d/1wbu92TyTiSaLgA7t1qI1L_d20za8OY1KMIDzeHzODfs/edit?usp=sharing', title: `my cv`, section: false},
+    {link: 'https://docs.google.com/spreadsheets/d/158m1-OMbBdhmgMDWOHeIC2AHIQql_a8n3v02BswpItI/edit?usp=sharing', title: `my reviews`, section: false},
     {link: 'lets-talk', title: `lets talk`, section: true}
 ]
 
